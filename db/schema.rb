712646940a4f6fd0d315d6c325e9702d9bc8f22f
aurelien canonne <ryuichiro@outlook.fr>
2021-08-24 17:35:55 +0200

    Home page, search bar, search js effectué

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_152542) do
+ActiveRecord::Schema.define(version: 2021_08_24_143244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_152542) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_missions_on_user_id"
   end
 
