ac3c0f7d80cfe39f4e94c098b0aa48e55760667b
Raphael Aubry <raphael.aubry@hotmail.fr>
2021-08-30 23:58:35 +0200

    more

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,9 +30,14 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { dashboard } from '../components/dashboard';
 
+
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
   dashboard();
+
+
+
 });
