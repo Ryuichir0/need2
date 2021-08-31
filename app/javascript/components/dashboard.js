766b9ddcd30cd8c3dd599b7105b470c32f5f1c37
Raphael Aubry <raphael.aubry@hotmail.fr>
2021-08-31 18:10:56 +0200

    done

--- app/javascript/components/dashboard.js
+++ app/javascript/components/dashboard.js
@@ -1,18 +1,24 @@
 const dashboard = () => {
   const need = document.querySelector("#need");
   const help = document.querySelector("#help");
-  
+  const needtitle = document.querySelector("#requetes");
+  const helptitle = document.querySelector("#offres");
+
   const div_need = document.getElementById("div_need");
   const div_help = document.getElementById("div_help");
 
   need.addEventListener("click", (event) => {
     div_need.classList.remove("d-none");
     div_help.classList.add("d-none");
+    needtitle.classList.add("title-toggle");
+    helptitle.classList.remove("title-toggle");
   });
 
   help.addEventListener("click", (event) => {
     div_help.classList.remove("d-none");
     div_need.classList.add("d-none");
+    needtitle.classList.remove("title-toggle");
+    helptitle.classList.add("title-toggle");
   });
 }
 export { dashboard }
