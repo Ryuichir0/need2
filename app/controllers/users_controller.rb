3567d89137c39499c49251257373d7ce560e9741
aurelien canonne <ryuichiro@outlook.fr>
2021-08-31 11:17:26 +0200

    Affichage profil helpers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(pseudo: params[:pseudo])
+    if current_user == @user
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+
+end
